93176c755cdfef15576cc29422e5e98406cd85e7
Steve Jorgensen <stevej@stevej.name>
2013-08-29 10:32:18 -0700

    Show help element on focus.

--- src/PhoneItIn.js
+++ src/PhoneItIn.js
@@ -2,7 +2,14 @@ var PhoneItIn = (function(){
   function UI(){
   }
   UI.prototype = (function(){
+    function addHelpToInput(input){
+      var helpEl = document.createElement('DIV');
+      helpEl.setAttribute('id', 'phin-help');
+      input.parentNode.insertBefore(helpEl, input.nextChild);
+    };
+
     function bindToInput(input){
+      input.addEventListener( 'focus', function(){ addHelpToInput(input); } );
     }
 
     return {
